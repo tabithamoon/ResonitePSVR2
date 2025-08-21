8f2524fe4a7b907245b7f19fa5349a096f97b6cf
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2025-08-21 11:36:12 -0300

    Licensed under MIT, upgraded version to 1.0.1

--- ResonitePSVR2/Properties/AssemblyInfo.cs
+++ ResonitePSVR2/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyProduct("ResonitePSVR2")]
 [assembly: AssemblyDescription("Enable special features for PlayStation VR2")]
 [assembly: AssemblyCompany("tabithamoon")]
-[assembly: AssemblyCopyright("AGPL-v3")]
+[assembly: AssemblyCopyright("MIT")]
 [assembly: AssemblyVersion(ResonitePSVR2.ResonitePSVR2.VERSION_CONSTANT)]
 [assembly: AssemblyFileVersion(ResonitePSVR2.ResonitePSVR2.VERSION_CONSTANT)]
