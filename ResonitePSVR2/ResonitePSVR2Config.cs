d2a9eab5e1736d8c338a42099495f2c30b0e61e2
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2025-08-27 09:42:56 -0300

    Removed option for smooth binary blinking
    See #1

--- ResonitePSVR2/ResonitePSVR2Config.cs
+++ ResonitePSVR2/ResonitePSVR2Config.cs
@@ -7,9 +7,4 @@ public partial class ResonitePSVR2 : ResoniteMod {
 	[AutoRegisterConfigKey]
 	internal static ModConfigurationKey<bool> EnableEyeTracking_Config = new("eye_tracking", "Enable eye tracking", () => true );
 	public static bool EnableEyeTracking => Config!.GetValue(EnableEyeTracking_Config);
-
-	[AutoRegisterConfigKey]
-	internal static ModConfigurationKey<bool> EnableSmoothBlinking_Config = new("smooth_blinking", "Smoothen out binary blinking");
-
-	public static bool EnableSmoothBlinking => Config!.GetValue(EnableSmoothBlinking_Config);
 }
