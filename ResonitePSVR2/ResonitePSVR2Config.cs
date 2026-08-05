2b8d1e3512012f7830ccd0f96c2237d2ee4e6d21
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2026-08-05 19:45:37 -0300

    Removed options for blink estimation It is no longer supported by PSVR2Toolkit, and it never worked very well...

--- ResonitePSVR2/ResonitePSVR2Config.cs
+++ ResonitePSVR2/ResonitePSVR2Config.cs
@@ -11,11 +11,7 @@ public partial class ResonitePSVR2 : ResoniteMod {
 	[AutoRegisterConfigKey]
 	internal static ModConfigurationKey<bool> EnableBlinkFiltering_Config = new("blink_flitering", "Enable blink smoothing", () => true );
 	public static bool EnableBlinkFiltering => Config!.GetValue(EnableBlinkFiltering_Config);
-	
-	[AutoRegisterConfigKey]
-	internal static ModConfigurationKey<bool> EnableEyeLidEstimation_Config = new("eyelid_estimation", "Enable eye lid estimation", () => false );
-	public static bool EnableEyeLidEstimation => Config!.GetValue(EnableEyeLidEstimation_Config);
-	
+
 	[AutoRegisterConfigKey]
 	internal static ModConfigurationKey<float> BlinkFilteringSpeed_Config = new("blink_filtering_speed", "Blink filtering speed", () => 20f);
 	public static float BlinkFilteringSpeed => Config!.GetValue(BlinkFilteringSpeed_Config);
